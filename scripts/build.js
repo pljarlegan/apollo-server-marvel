d8f8629627d72b68bbed4183314537c4940ae4cc
pljarlegan <pl.jarlegan@citae.fr>
2019-10-20 18:52:58 +0200

    ADD: docker config

---
+++ scripts/build.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+const util = require("util"),
+  exec = util.promisify(require("child_process").exec),
+  packageConfig = require("../package");
+
+async function build() { // eslint-disable-line func-style
+  "use strict";
+  const { stdout, stderr } = await exec(`docker build -f node/Dockerfile -t ${packageConfig.docker.owner}/${packageConfig.name}:${packageConfig.version} --no-cache .`);
+  if (stderr) {
+    console.log(stderr);
+    return;
+  }
+  console.log(stdout);
+}
+build();
