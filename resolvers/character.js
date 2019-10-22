3683ceef0175f08a504014bba3f0b095924cf45c
pljarlegan <pl.jarlegan@citae.fr>
2019-10-22 14:11:45 +0200

    MIN: remove unnecessary default values

--- resolvers/character.js
+++ resolvers/character.js
@@ -2,7 +2,7 @@ const { ApolloError } = require("apollo-server");
 
 const resolvers = {
   Query: {
-    characters: ( parent, { limit = 10, offset = 0 }, { dataSources: { CharacterRESTDataSource } }, info) => CharacterRESTDataSource.characters({ limit, offset }),
+    characters: ( parent, { limit, offset }, { dataSources: { CharacterRESTDataSource } }, info) => CharacterRESTDataSource.characters({ limit, offset }),
     character: ( parent, { id }, { dataSources: { CharacterRESTDataSource } }, info ) => CharacterRESTDataSource.findCharacterById(id).then(character => character ? character : new ApolloError("Character not found.", "RESOURCE_NOT_FOUND")),
   },
 
